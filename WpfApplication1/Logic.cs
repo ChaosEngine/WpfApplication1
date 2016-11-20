ca906e4198b84cd289c4a7b501f842af32f1c9a2
chaos <chaos@WIN10VM>
2016-11-20 17:45:32 +0100

    initial commit

---
+++ WpfApplication1/Logic.cs
@@ -0,0 +1,323 @@
+﻿using MySql.Data.MySqlClient;
+using Npgsql;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace WpfApplication1
+{
+    public enum DatabaseTypeEnum { MYSQL, POSTGRES, MSSQL, MSSQL_ENTITIES }
+
+    public interface ISqlTestExecutor
+    {
+        StringBuilder Execute();
+    }
+
+    public abstract class SqlTesterBase : ISqlTestExecutor
+    {
+        public enum TestTypeEnum { GROUPEDVIEW, SEARCHER }
+
+        protected string _sqlQuery { get; set; }
+
+        public DatabaseTypeEnum DataBaseType { get; protected set; }
+
+        public TestTypeEnum TestType { get; protected set; }
+
+        public SqlTesterBase(DatabaseTypeEnum dataBaseType, TestTypeEnum testType)
+        {
+            DataBaseType = dataBaseType;
+            TestType = testType;
+        }
+
+        protected abstract StringBuilder DoWorkMysql(SqlTester data);
+
+        protected abstract StringBuilder DoWorkPostgres(SqlTester data);
+
+        protected abstract StringBuilder DoWorkMssql(SqlTester data);
+
+        protected abstract StringBuilder DoWorkEntities(SqlTester data);
+
+        protected abstract StringBuilder ExecuteGroupedView();
+
+        protected abstract StringBuilder ExecuteSearcher();
+
+        public StringBuilder Execute()
+        {
+            switch (TestType)
+            {
+                case TestTypeEnum.GROUPEDVIEW:
+                    return ExecuteGroupedView();
+                case TestTypeEnum.SEARCHER:
+                    return ExecuteSearcher();
+                default:
+                    throw new NotSupportedException("bad test type");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Used to pass custom args into the worker function.
+    /// </summary>
+    public class SqlTester : SqlTesterBase
+    {
+        int? _initialSleep;
+        Random _rnd;
+        string _keyword2Search = null;
+        int _maxCount = 3000;
+
+        public SqlTester(DatabaseTypeEnum dataBaseType, TestTypeEnum testType)
+            : base(dataBaseType, testType)
+        {
+    
+            _rnd = new Random((int)DateTime.Now.Ticks);
+            _initialSleep = null;
+        }
+
+        string GetRandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder(size);
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * _rnd.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+
+        protected override StringBuilder ExecuteGroupedView()
+        {
+            StringBuilder result;
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            switch (DataBaseType)
+            {
+                case DatabaseTypeEnum.MYSQL:
+                    _sqlQuery = string.Concat("SELECT domain, cnt, now() FROM domains limit ", _maxCount);
+                    result = this.DoWorkMysql(this);
+                    break;
+                case DatabaseTypeEnum.POSTGRES:
+                    _sqlQuery = string.Concat("SELECT domain, cnt, now() FROM domains limit ", _maxCount);
+                    result = this.DoWorkPostgres(this);
+                    break;
+                case DatabaseTypeEnum.MSSQL:
+                    _sqlQuery = string.Concat("SELECT top ", _maxCount, " domain, cnt, getdate() FROM domains");
+                    result = this.DoWorkMssql(this);
+                    break;
+                case DatabaseTypeEnum.MSSQL_ENTITIES:
+                    result = this.DoWorkEntitiesGroupedView(this);
+                    break;
+                default:
+                    throw new NotSupportedException("Bad DataBaseType");
+            }
+            timer.Stop();
+            result.Insert(0, string.Concat("elapsed ", timer.ElapsedMilliseconds, " milliseconds", Environment.NewLine));
+            return result;
+        }
+
+        protected override StringBuilder ExecuteSearcher()
+        {
+            StringBuilder result;
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            _keyword2Search = GetRandomString(3).ToLower();
+            switch (DataBaseType)
+            {
+                case DatabaseTypeEnum.MYSQL:
+                    _sqlQuery = string.Concat(@"select user_name, clear_passwd, user_id, now() 
+                                                from accounts
+                                                where clear_passwd like '%", _keyword2Search, "%'");
+                    result = this.DoWorkMysql(this);
+                    break;
+                case DatabaseTypeEnum.POSTGRES:
+                    _sqlQuery = string.Concat(@"select user_name, clear_passwd, cast(user_id as bigint), now()
+                                                from accounts
+                                                where clear_passwd like '%", _keyword2Search, "%'");
+                    result = this.DoWorkPostgres(this);
+                    break;
+                case DatabaseTypeEnum.MSSQL:
+                    _sqlQuery = string.Concat(@"select user_name, clear_passwd, cast(user_id as bigint), getdate() 
+                                                from accounts 
+                                                where clear_passwd like '%", _keyword2Search, "%'");
+                    result = this.DoWorkMssql(this);
+                    break;
+                case DatabaseTypeEnum.MSSQL_ENTITIES:
+                    result = this.DoWorkEntitiesSearcher(this);
+                    break;
+                default:
+                    throw new NotSupportedException("Bad DataBaseType");
+            }
+            timer.Stop();
+            result.Insert(0, string.Concat("for ", _keyword2Search, " elapsed ", timer.ElapsedMilliseconds, " millis",
+                Environment.NewLine));
+            return result;
+        }
+
+        [Obsolete]
+        void ParseReaderSimple(IDataReader rdr, StringBuilder sb)
+        {
+            sb.Append(rdr.GetString(0)).Append(" | ")
+                .Append(rdr.GetInt64(1)).Append(" | ")
+                .Append(rdr.GetDateTime(2))
+                .AppendLine();
+        }
+
+        void ParseReaderAll(IDataReader rdr, StringBuilder sb)
+        {
+            string comma = string.Empty;
+            int count = rdr.FieldCount;
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(comma)
+                    .Append(rdr[i].ToString());
+                comma = " | ";
+            }
+            sb.AppendLine();
+        }
+
+        void ProcessReader(IDataReader rdr, StringBuilder sb)
+        {
+            //uint i = 0;
+            while (rdr.Read())
+            {
+                ParseReaderAll(rdr, sb);
+                //i++;
+            }
+            //sb.Append("Got ").Append(i).Append(" rows");
+        }
+
+        protected override StringBuilder DoWorkMysql(SqlTester data)
+        {
+            if (data._initialSleep.HasValue)
+                Thread.Sleep(data._initialSleep.Value);
+            using (var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["Mysql"].ConnectionString))
+            {
+                conn.Open();
+
+                var sb = new StringBuilder(100000);
+                using (MySqlCommand cmd = new MySqlCommand(data._sqlQuery, conn))
+                {
+                    cmd.Prepare();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    using (MySqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        ProcessReader(rdr, sb);
+                        return sb;
+                    }
+                }
+            }
+        }
+
+        protected override StringBuilder DoWorkPostgres(SqlTester data)
+        {
+            if (data._initialSleep.HasValue)
+                Thread.Sleep(data._initialSleep.Value);
+            using (var conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString))
+            {
+                conn.Open();
+
+                var sb = new StringBuilder(1000);
+                using (var cmd = new NpgsqlCommand(data._sqlQuery, conn))
+                {
+                    cmd.Prepare();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    using (NpgsqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        ProcessReader(rdr, sb);
+                        return sb;
+                    }
+                }
+            }
+        }
+
+        protected override StringBuilder DoWorkMssql(SqlTester data)
+        {
+            if (data._initialSleep.HasValue)
+                Thread.Sleep(data._initialSleep.Value);
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Mssql"].ConnectionString))
+            {
+                conn.Open();
+
+                var sb = new StringBuilder(1000);
+                using (var cmd = new SqlCommand(data._sqlQuery, conn))
+                {
+                    cmd.Prepare();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        ProcessReader(rdr, sb);
+                        return sb;
+                    }
+                }
+            }
+        }
+
+        protected override StringBuilder DoWorkEntities(SqlTester data)
+        {
+            switch (TestType)
+            {
+                case TestTypeEnum.GROUPEDVIEW:
+                    return DoWorkEntitiesGroupedView(data);
+                case TestTypeEnum.SEARCHER:
+                    return DoWorkEntitiesSearcher(data);
+                default:
+                    throw new NotSupportedException("bad test type");
+            }
+        }
+
+        StringBuilder DoWorkEntitiesGroupedView(SqlTester data)
+        {
+            if (data._initialSleep.HasValue)
+                Thread.Sleep(data._initialSleep.Value);
+            using (testEntities context = new testEntities())
+            {
+                var query = (from e in context.GetDomains(data._maxCount)
+                             select e);
+
+                var sb = new StringBuilder(1000);
+                foreach (var rdr in query.AsParallel())
+                {
+                    sb.Append(rdr.domain).Append(" | ")
+                        .Append(rdr.cnt).Append(" | ")
+                        .Append(rdr.now)
+                        .AppendLine();
+                }
+                return sb;
+            }
+        }
+
+        StringBuilder DoWorkEntitiesSearcher(SqlTester data)
+        {
+            if (data._initialSleep.HasValue)
+                Thread.Sleep(data._initialSleep.Value);
+            using (testEntities context = new testEntities())
+            {
+                var query = (from acc in context.accounts
+                             where acc.clear_passwd.Contains(data._keyword2Search)
+                             select new
+                             {
+                                 user_name = acc.user_name,
+                                 clear_passwd = acc.clear_passwd,
+                                 user_id = acc.user_id,
+                                 now = DateTime.Now
+                             });
+
+                var sb = new StringBuilder(1000);
+                foreach (var rdr in query.AsParallel())
+                {
+                    sb.Append(rdr.user_name).Append(" | ")
+                        .Append(rdr.clear_passwd).Append(" | ")
+                        .Append(rdr.user_id).Append(" | ")
+                        .Append(rdr.now)
+                        .AppendLine();
+                }
+                return sb;
+            }
+        }
+    }//end class
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,318 @@
+﻿using MySql.Data.MySqlClient;
+using Npgsql;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        interface IResultPrinterControl
+        {
+            string Text { get; set; }
+
+            void AppendText(string textData);
+        }
+
+        class ResultPrinter : IResultPrinterControl
+        {
+            dynamic _resultPrinterImplementator;
+            object _syncer;
+
+            public ResultPrinter(dynamic resultPrinterImplementator)
+            {
+                _resultPrinterImplementator = resultPrinterImplementator;
+                _syncer = new object();
+            }
+
+            public string Text
+            {
+                get
+                {
+                    lock (_syncer)
+                    {
+                        return _resultPrinterImplementator.Text;
+                    }
+                }
+                set
+                {
+                    lock (_syncer)
+                    {
+                        _resultPrinterImplementator.Text = value;
+                    }
+                }
+            }
+
+            public void AppendText(string value)
+            {
+                lock (_syncer)
+                {
+                    _resultPrinterImplementator.AppendText(value);
+                }
+            }
+        }
+
+        IResultPrinterControl OutputResultControl { get; set; }
+
+        DatabaseTypeEnum SelectedDBaseType
+        {
+            get
+            {
+                if (rbDBaseMysql.IsChecked.GetValueOrDefault(false))
+                    return DatabaseTypeEnum.MYSQL;
+                else if (rbDBasePostgres.IsChecked.GetValueOrDefault(false))
+                    return DatabaseTypeEnum.POSTGRES;
+                else if (rbDBaseMssql.IsChecked.GetValueOrDefault(false))
+                    return DatabaseTypeEnum.MSSQL;
+                else if (rbDBaseMssqlEntities.IsChecked.GetValueOrDefault(false))
+                    return DatabaseTypeEnum.MSSQL_ENTITIES;
+                else
+                    throw new NotSupportedException("Bad DataBaseType");
+            }
+        }
+
+        public SqlTesterBase.TestTypeEnum TestType
+        {
+            get
+            {
+                if (cmbTestType.SelectionBoxItem.ToString().ToUpper() == SqlTesterBase.TestTypeEnum.GROUPEDVIEW.ToString())
+                    return SqlTesterBase.TestTypeEnum.GROUPEDVIEW;
+                else if (cmbTestType.SelectionBoxItem.ToString().ToUpper() == SqlTesterBase.TestTypeEnum.SEARCHER.ToString())
+                    return SqlTesterBase.TestTypeEnum.SEARCHER;
+                else
+                    throw new NotSupportedException("Bad DataBaseType");
+            }
+        }
+
+        public int? ConcurrencyCount
+        {
+            get
+            {
+                int i;
+                if (int.TryParse(((ListBoxItem)cmbConcurrentyCount.SelectedItem).Content.ToString(), out i))
+                {
+                    return i;
+                }
+                return null;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            OutputResultControl = new ResultPrinter(txtDebug);
+        }
+
+        async Task<StringBuilder> DoWorkAsync(SqlTester data)
+        {
+            //StringBuilder sb = DoWork(data);
+            //return sb;
+            var task = Task.Factory.StartNew(() =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    OutputResultControl.AppendText("...working with...." + data.DataBaseType.ToString() + Environment.NewLine);
+                });
+                return data.Execute();
+            });
+            return await task;
+        }
+
+        // Three things to note in the signature: 
+        //  - The method has an async modifier.  
+        //  - The return type is Task or Task<T>. (See "Return Types" section.)
+        //    Here, it is Task<int> because the return statement returns an integer. 
+        //  - The method name ends in "Async."
+        async Task<StringBuilder> AccessTheWebAsync()
+        {
+            // You need to add a reference to System.Net.Http to declare client.
+            HttpClient client = new HttpClient();
+
+            // GetStringAsync returns a Task<string>. That means that when you await the 
+            // task you'll get a string (urlContents).
+            Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
+
+            // You can do work here that doesn't rely on the string from GetStringAsync.
+            OutputResultControl.AppendText("Working . . . . . . ." + Environment.NewLine);
+
+            // The await operator suspends AccessTheWebAsync. 
+            //  - AccessTheWebAsync can't continue until getStringTask is complete. 
+            //  - Meanwhile, control returns to the caller of AccessTheWebAsync. 
+            //  - Control resumes here when getStringTask is complete.  
+            //  - The await operator then retrieves the string result from getStringTask. 
+            string urlContents = await getStringTask;
+
+            // The return statement specifies an integer result. 
+            // Any methods that are awaiting AccessTheWebAsync retrieve the length value. 
+            return new StringBuilder(urlContents);
+        }
+
+        private void ch_old_Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OutputResultControl.Text = "starting Task.Factory.StartNew";
+            ((Button)sender).IsEnabled = false;
+
+            Task.Factory.StartNew(() =>
+            {
+                // Do your query in here - just simulating work with a sleep.
+                SqlTester data = null;
+                Dispatcher.Invoke(() =>
+                {
+                    data = new SqlTester(SelectedDBaseType, TestType);
+                    OutputResultControl.AppendText("...working with...." + data.DataBaseType.ToString() + Environment.NewLine);
+                });
+                try
+                {
+                    var sb = data.Execute();
+                    // Note: you can't access the UI directly here in the worker thread. Use
+                    // Form.Invoke() instead to update the UI after your work is done.  
+                    Dispatcher.Invoke(() =>
+                    {
+                        OutputResultControl.Text = sb.ToString();
+                        ((Button)sender).IsEnabled = true;
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        OutputResultControl.Text = "Error: " + ex.Message;
+                        ((Button)sender).IsEnabled = true;
+                    });
+                }
+            });
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OutputResultControl.Text = "starting multipple concurrency test" + Environment.NewLine;
+                var db_type = SelectedDBaseType;
+                var tst_type = TestType;
+                ((Button)sender).IsEnabled = false;
+                int concurrentyCount = ConcurrencyCount.GetValueOrDefault(1);
+
+                Task.Factory.StartNew(() =>
+                {
+                    Parallel.For(0, concurrentyCount, (i) =>
+                    {
+                        try
+                        {
+                            var data = new SqlTester(db_type, tst_type);
+                            Dispatcher.Invoke(() =>
+                            {
+                                OutputResultControl.AppendText("starting test #" + i + " with " + data.DataBaseType.ToString() +
+                                    Environment.NewLine);
+                            });
+
+                            var result = data.Execute();
+
+                            // Display the result. All is ok
+                            Dispatcher.Invoke(() =>
+                            {
+                                OutputResultControl.Text = result.ToString();
+                            });
+                        }
+                        catch (Exception ex)
+                        {
+                            //Show error
+                            Dispatcher.Invoke(() =>
+                            {
+                                OutputResultControl.Text = "Error: " + ex.Message;
+                                ((Button)sender).IsEnabled = true;
+                            });
+                        }
+                    });
+                    //All sub-task ended. Notify UI
+                    Dispatcher.Invoke(() =>
+                    {
+                        ((Button)sender).IsEnabled = true;
+                    });
+                });
+            }
+            catch (Exception ex)
+            {
+                //Show error
+                OutputResultControl.Text = "Error: " + ex.Message;
+                ((Button)sender).IsEnabled = true;
+            }
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Task<StringBuilder> async_task = DoWorkAsync(new SqlTester(SelectedDBaseType, TestType));
+                OutputResultControl.Text = "starting await select";
+                ((Button)sender).IsEnabled = false;
+                StringBuilder result = await async_task;
+
+                // Display the result. All is ok
+                OutputResultControl.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                //Show error
+                OutputResultControl.Text = "Error: " + ex.Message;
+            }
+            finally
+            {
+                ((Button)sender).IsEnabled = true;
+            }
+        }
+
+        private async void AsyncWeb_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ((Button)sender).IsEnabled = false;
+                Task<StringBuilder> async_task = AccessTheWebAsync();
+                OutputResultControl.Text = "starting AccessTheWebAsync!";
+
+                StringBuilder contentLength = await async_task;
+
+                OutputResultControl.Text = contentLength.ToString();
+            }
+            catch (Exception ex)
+            {
+                //Show error
+                OutputResultControl.Text = "Error: " + ex.Message;
+            }
+            finally
+            {
+                ((Button)sender).IsEnabled = true;
+            }
+        }
+
+        private void Window_KeyUp_1(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Escape)
+                Close();
+        }
+
+        private void cmbConcurrentyCount_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int? i = this.ConcurrencyCount;
+            if (i.HasValue)
+            {
+                btnAsync.IsEnabled = i.Value <= 1;
+            }
+            else
+                btnAsync.IsEnabled = false;
+        }
+    }
+}
